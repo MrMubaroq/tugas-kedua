1609b9e113520e284ec051adb532f772a37b247c
MrMubaroq <mubaroqahmad89@gmail.com>
2024-10-20 10:39:46 +0800

    commit kedua

---
+++ main.js
@@ -0,0 +1,11 @@
+let Umur = prompt("input kategori");
+
+if (Umur <= 12) {
+    console.log("Anak-Anak");
+}else if (Umur <= 17) {
+    console.log("Remaja");
+}else if (Umur <= 64) {
+    console.log ("Dewasa");
+}else if (Umur <= 100) {
+    console.log("Lansia");
+};
